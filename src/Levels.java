2e03d7a0c7b8ca5b38c03370ead10dbb69c6a691
unknown <dat13mbe@student.lu.se>
2017-05-07 16:27:11 +0200

    Added comment with the different level speeds

--- src/Levels.java
+++ src/Levels.java
@@ -6,6 +6,15 @@ import javax.swing.Timer;
  * Class to bind levels to drop speed
  * 
  * DEFAULT SPEED (index = 1) - 400ms
+ * Level number: 2 has speed of : 363
+ * Level number: 3 has speed of : 326
+ * Level number: 4 has speed of : 289
+ * Level number: 5 has speed of : 252
+ * Level number: 6 has speed of : 215
+ * Level number: 7 has speed of : 178
+ * Level number: 8 has speed of : 141
+ * Level number: 9 has speed of : 104
+ * Level number: 10 has speed of : 67
  * 
  * @author Martin
  *
@@ -22,7 +31,7 @@ public class Levels {
 		setUp();
 		System.out.println(map.get(10));
 	}
-	
+
 	private void setUp(){
 		int speed = 400; //400ms
 		map.put(1, speed);
@@ -31,19 +40,19 @@ public class Levels {
 			map.put(i, speed);
 		}
 	}
-	
+
 	public void levelUp(){
 		if(currentLevel != maxLevels){
 			currentLevel++;
 			timer.setDelay(map.get(currentLevel));
 		}
 	}
-	
+
 	public void restoreLevels(){
 		currentLevel = 1;
 		timer.setDelay(map.get(currentLevel));
 	}
-	
+
 	public int getCurrentLevel(){
 		return currentLevel;
 	}
